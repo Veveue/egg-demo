95170819181ab5ef96d862a2571f66a754373435
veveue <veveue@dingtalk.com>
2018-09-27 19:56:35 +0800

    -

--- config/plugin.js
+++ config/plugin.js
@@ -1,8 +1,11 @@
 // had enabled by egg
-// exports.static = true;
+exports.static = true;
 
-// ejs
 exports.ejs = {
   enable: true,
   package: "egg-view-ejs",
 };
+// exports.react = {
+//   enable: true,
+//   package: "egg-view-react",
+// };
